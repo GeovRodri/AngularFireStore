de21843a03e14fc1e010cfbdf6774c155689d25c
geovany <geovanyscv@gmail.com>
2019-06-19 07:32:43 -0300

    exemple angular firestore

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { AngularFirestore } from 'angularfire2/firestore';
-import { AngularFireDatabase } from 'angularfire2/database';
 import { AngularFireAuth } from 'angularfire2/auth';
 import { Observable } from 'rxjs/Observable';
 import * as firebase from 'firebase/app';
@@ -11,16 +10,40 @@ import * as firebase from 'firebase/app';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  user: Observable<firebase.User>;
-  items: Observable<any[]>;
-  msgVal: string = '';
+  user: any;
+  items: any;
 
   constructor(public afAuth: AngularFireAuth, db: AngularFirestore) {
     this.afAuth.auth.signInAnonymously();
 
     this.user = this.afAuth.authState;
-    this.items = db.collection('items').valueChanges();
 
+    const data = {
+      'empresa_id': db.doc('/empresas/wTpgOJrLFT1qBJq4PdT9').ref,
+      'usuario_id': db.doc('/usuarios/mg1OpXYRVhikpkralPnr').ref
+    };
+
+    db.collection('empresas_usuarios').add(data);
+
+    db.collection('empresas_usuarios').doc('U9BiIa6gvGZVhbLC9vHf').valueChanges().subscribe((data: any) => {
+      data.empresa_id.get().then((result) => {
+        console.log('Document - Empresa: ', result.data());
+      });
+
+      data.usuario_id.get().then((result) => {
+        console.log('Document - Usuario:', result.data());
+      });
+    });
+
+    db.collection('empresas_usuarios', ref => ref.where('empresa_id/nome', '==', 'Stay')).valueChanges().subscribe((data: any) => {
+      console.log('Where : ', data);
+      /*data.empresa_id.get().then((result) => {
+        console.log('Where - Empresa: ', result.data());
+      });
+
+      data.usuario_id.get().then((result) => {
+        console.log('Where - Usuario:', result.data());
+      });*/
+    });
   }
-
 }
